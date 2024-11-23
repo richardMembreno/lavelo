bad7d89528cfa918ef04d842a4e05a4c6b670400
richardMembreno <68753173+richardMembreno@users.noreply.github.com>
2024-11-23 15:19:35 -0600

    cambio final

--- src/components/Invitation/index.js
+++ src/components/Invitation/index.js
@@ -5,7 +5,9 @@ const Invitation = ({ param }) => {
     const [data, setData] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
-    
+    const [isConfirmed, setIsConfirmed] = useState(false);
+    const [dateConfirmation, setDateConfirmation] = useState('');
+
     useEffect(() => {
         const apiUrl = `https://wedding-api-gnabf5bsbnd2dcd6.eastus-01.azurewebsites.net/Invitation/GetGuest?id=${param}`;
 
@@ -17,6 +19,16 @@ const Invitation = ({ param }) => {
                 }
                 const data = await response.json();
                 setData(data);
+               // Verificamos si DateConfirmation es null o undefined 
+                if (data.dateConfirmation) 
+                { 
+                    setDateConfirmation('Ya has enviado una respuesta!');
+                } 
+
+                if(data.statusConfirmation){
+                    setIsConfirmed(data.statusConfirmation);
+                }
+                
             }catch(error){
                 setError(error);
             }finally{
@@ -26,6 +38,37 @@ const Invitation = ({ param }) => {
 
         fetchData();
     }, [param]);
+
+    const handleOptionChange = (event) => {
+        setIsConfirmed(event.target.value === 'si');
+    }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const payload = { IdGuest: param, isConfirmed};
+
+        try{
+            const response = await fetch('https://wedding-api-gnabf5bsbnd2dcd6.eastus-01.azurewebsites.net/Invitation/UpdateGuest',{
+               method: 'POST',
+               headers: {
+                'Content-Type': 'application/json'
+               },
+               body: JSON.stringify(payload)
+            });
+
+            if(!response.ok){
+                console.log(payload);
+                throw new Error('Error en la solicitud');
+            }
+            const resp = await response.json();
+            if(resp.code === 1){
+                setDateConfirmation('Respuesta enviada.');
+            }
+        }catch(error){
+            console.log('Error al enviar la solicitud:', error);
+        }
+
+    };
 
     if(loading){
         return <div>Cargando Invitación...</div>;
@@ -48,6 +91,28 @@ const Invitation = ({ param }) => {
                                 <div >
                                     <p>Hola {data.guestName}. Queremos compartir contigo este momento especial y te hemos reservado un lugar para {data.quantityCompanions} {data.quantityCompanions > 1 ? 'personas' : 'persona'}.</p>
                                 </div>
+                                <form onSubmit={handleSubmit}>
+                                    <div>
+                                        <p>Por favor, confirmar asistencia:</p>
+                                    </div>
+                                    <div className='radio-group'>
+                                        <label className='radio-container'>
+                                            Si
+                                            <input type='radio' value="si" checked={isConfirmed === true} onChange={handleOptionChange} />
+                                            <span className="checkmark"></span>
+                                        </label>
+
+                                        <label className='radio-container'>
+                                            No
+                                            <input type='radio' value="no" checked={isConfirmed === false} onChange={handleOptionChange} />
+                                            <span className="checkmark"></span>
+                                        </label>
+                                    </div>
+                                    <div className="next-post">
+                                        <button href="/#" type='submit'><span className="post-control-link">Enviar</span></button>
+                                    </div>
+                                </form>
+                                <p>{dateConfirmation}</p>
                             </div>
                         </div>
                     </div>
